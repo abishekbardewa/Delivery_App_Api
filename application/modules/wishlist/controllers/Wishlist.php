4b1d02816ee578d4de7d6b908801a5c31856d212
abishekbardewa <way2abishek@gmail.com>
2020-10-19 20:07:25 +0530

    Wishlist added and some changes on Cart also

--- application/modules/wishlist/controllers/Wishlist.php
+++ application/modules/wishlist/controllers/Wishlist.php
@@ -1,54 +1,75 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Wishlist extends MX_Controller
 {
-    function __construct(){
-        parent::__construct();
-        $this->load->module('customer_activity');
-    }
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->module('customer_activity');
+	}
 	function index()
 	{
-		$data['title']='Wish List';
-		$data['module']='wishlist';
-		$data['view_file']='wishlist';
-		echo Modules::run('template/layout2',$data);
+		$data['title'] = 'Wish List';
+		$data['module'] = 'wishlist';
+		$data['view_file'] = 'wishlist';
+		echo Modules::run('template/layout2', $data);
 	}
-		
-    function count(){
-        if(@$_SESSION['user_id']){
-            echo $this->db->where('user_id',@$_SESSION['user_id'])->get('wishlist')->num_rows();
-        }else echo 0;
+	function view()
+	{
+		if (@$_SESSION['user_id']) {
+			$where = $this->session->user_id;
+			$this->db->join('products', 'p_id');
+			$return = $this->db->select("p_id,name,img,sp")
+				->where('user_id', $where)
+				->order_by('id', "DESC")
+				->get('wishlist');
+
+			$this->output->set_content_type('application/json')->set_output(json_encode($return->result_array()));
+		}
 	}
-	function add($pid){
-	    if(@$_SESSION['user_id']){
-    	    if($pid){
-    	        $data = array(
-    	            'p_id'      => $pid,
-    	            'user_id'     => $_SESSION['user_id']
-    	        );
-        		if($this->db->where($data)->get('wishlist')->num_rows()>0){
-        		    echo "Item <b>Already added</b> to wishlist";die();
-        		}
-        		$this->customer_activity->add('Wishlist Added',array('pid'=>$pid));
-        		echo $this->db->insert('wishlist',$data);
-    	    }else{
-    	        echo "Invalid request";
-    	    }
-	    }else{
-	        echo 'Please <b><a href="#signin-modal" data-toggle="modal">Sign in</a></b> to add wishlists';
-	    }
+
+	function count()
+	{
+		if (@$_SESSION['user_id']) {
+			$return = $this->db->where('user_id', @$_SESSION['user_id'])->get('wishlist');
+			$this->output->set_content_type('application/json')->set_output(json_encode($return->num_rows()));
+		} else {
+			echo "0";
+		}
 	}
-    function delete($pid)
-    {
-    	$this->db->where('user_id',@$_SESSION['user_id'])->where('p_id',$pid);
-       	if($this->db->delete('wishlist')){
-       	    $this->customer_activity->add('Wishlist Removed',array('pid'=>$pid));
-       	    $_SESSION['msg']="Product successfully remove from your wishlist";
-       	    $this->session->mark_as_flash('msg');
-       		redirect("users/wishlist");
-       	}else{
-       	    $_SESSION['msg']="Failed to remove from your wishlist";
-       	    $this->session->mark_as_flash('msg');
-       	    redirect("users/wishlist");
-       	}
-    }  
+	function add($pid)
+	{
+		if (@$_SESSION['user_id']) {
+			if ($pid) {
+				$data = array(
+					'p_id'      => $pid,
+					'user_id'     => $_SESSION['user_id']
+				);
+				if ($this->db->where($data)->get('wishlist')->num_rows() > 0) {
+					// echo "Item <b>Already added</b> to wishlist";
+					echo "3";
+					die();
+				}
+				// $this->customer_activity->add('Wishlist Added', array('pid' => $pid));
+				$this->db->insert('wishlist', $data);
+				echo "1";
+			} else {
+				echo "0";
+			}
+		} else {
+			echo "4";
+			// echo 'Please <b><a href="#signin-modal" data-toggle="modal">Sign in</a></b> to add wishlists';
+		}
+	}
+	function delete($pid)
+	{
+		$this->db->where('user_id', @$_SESSION['user_id'])->where('p_id', $pid);
+		if ($this->db->delete('wishlist')) {
+			// $this->customer_activity->add('Wishlist Removed', array('pid' => $pid));
+			// $_SESSION['msg'] = "Product successfully remove from your wishlist";
+			// $this->session->mark_as_flash('msg');
+			echo "1";
+		} else {
+			echo "0";
+		}
+	}
 }
